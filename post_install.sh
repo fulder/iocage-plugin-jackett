d1f2d303e011fc80ee24b1c14b174c58aa11e700
fulder <misad90@gmail.com>
2020-04-09 13:45:26 +0200

    Make jackett own the /usr/local/share/jackett folder

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Need to make jackett user own /usr/local/share/jackett folder
+# this is needed for update to work properly
+chown -R jackett /usr/local/share/jackett
+
 # Enable and start service
 
 sysrc jackett_enable=YES
