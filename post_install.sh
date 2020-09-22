eea13cbd241481394528cae198b59d50dfc97d36
fulder <misad90@gmail.com>
2020-09-23 00:10:57 +0200

    Correct echo in post_install

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Pathing mono to 6.8.0"
+echo "Patching mono to 6.8.0"
 pkg install -y /usr/local/migration/bin/mono-6.8.0.105.txz
 
 # Need to make jackett user own /usr/local/share/jackett folder
