503c51ada43725c8de604d6935c043e2b8132241
fulder <misad90@gmail.com>
2020-09-22 23:02:21 +0200

    Add same patch and cert sync to post_install

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "Pathing mono to 6.8.0"
+pkg install -y bin/mono-6.8.0.105.txz
+
+echo "Syncing CA certificates"
+cert-sync /usr/local/etc/ssl/cert.pem
+
 # Need to make jackett user own /usr/local/share/jackett folder
 # this is needed for update to work properly
 chown -R jackett /usr/local/share/jackett
