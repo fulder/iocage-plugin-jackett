304cb7637ee37fd8bbb9c49fc774e38d94dd7212
fulder <misad90@gmail.com>
2020-09-22 23:54:13 +0200

    Remove syncing CA certificates

--- post_install.sh
+++ post_install.sh
@@ -3,9 +3,6 @@
 echo "Pathing mono to 6.8.0"
 pkg install -y /usr/local/migration/bin/mono-6.8.0.105.txz
 
-echo "Syncing CA certificates"
-cert-sync /usr/local/etc/ssl/cert.pem
-
 # Need to make jackett user own /usr/local/share/jackett folder
 # this is needed for update to work properly
 chown -R jackett /usr/local/share/jackett
