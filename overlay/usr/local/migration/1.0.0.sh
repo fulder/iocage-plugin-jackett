9fb99a7f3c8e6e5eb323d6d2d437195ed7769323
fulder <misad90@gmail.com>
2021-10-16 00:03:39 +0200

    Make sure CA certs are synced

--- overlay/usr/local/migration/1.0.0.sh
+++ overlay/usr/local/migration/1.0.0.sh
@@ -5,5 +5,10 @@ echo "Running migration for 1.0.0"
 echo "Remove mono dependency"
 pkg remove -fy mono6.8
 
+echo "Update CA certs and resync"
+pkg install -y ca_root_nss
+cert-sync /usr/local/share/certs/ca-root-nss.crt
+
 echo "Patching mono to 6.8.0"
 pkg install -y /usr/local/migration/bin/mono-6.8.0.105.txz
+
